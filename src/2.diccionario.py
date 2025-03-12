7e77e9e1d5cc6655b54bbccd97e30e2b873bb341
Pablo <you@example.com>
2025-03-12 15:44:08 +0100

    contar vocales con bucle for y diccionario

---
+++ src/2.diccionario.py
@@ -0,0 +1,23 @@
+#contar vocales de diccionario, definimos funcion
+
+def contar_vocales(palabra):
+
+#creamos una lista con las bocales
+    vocales = "aeiou"
+
+#creamos diccionario, inicializamos a 0
+    resultado = {
+    "a" : 0,
+    "e" : 0,
+    "i" : 0,
+    "o" : 0,
+    "u" : 0,
+}
+
+    for letra in palabra:
+        if letra in vocales:
+            resultado[letra]+=1
+
+    return resultado
+
+print(contar_vocales("bucaramango"))        
